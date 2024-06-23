de96b086ffa1c697c8e9d73a3b2d858749176c4c
Evagriy <egorkoleda56@gmail.com>
2024-06-23 14:08:58 +0300

    the code for the movement of the snake

--- Begin project.py
+++ Begin project.py
@@ -1,3 +1,14 @@
-print("Hello, world")
+from typing import List, Tuple
 
-print("Hello, Kostaynchik")
+number_tuple = Tuple[int, int]
+
+list_numbers = [(2, 4), (5, 7)]
+new_tuple = (1, 2)
+
+def add_tuple(new_tuple: number_tuple) -> None:
+
+    list_numbers.append(new_tuple)
+    del list_numbers[0]
+    print(list_numbers)
+
+add_tuple(new_tuple)

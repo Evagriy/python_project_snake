76cca1d1cf26c761e98ea461ade5885bffe32f30
Evagriy <egorkoleda56@gmail.com>
2024-06-24 13:54:30 +0300

    I'm not sure that's how it should be.

--- Begin project.py
+++ Begin project.py
@@ -1,14 +1,15 @@
 from typing import List, Tuple
 
-number_tuple = Tuple[int, int]
-
 list_numbers = [(2, 4), (5, 7)]
 new_tuple = (1, 2)
-
-def add_tuple(new_tuple: number_tuple) -> None:
+eating:  bool = False
+not_eating: bool = True
+def add_tuple(new_tuple: Tuple[int, int]) -> None:
 
     list_numbers.append(new_tuple)
-    del list_numbers[0]
+    if not_eating:
+        del list_numbers[0]
+
     print(list_numbers)
 
 add_tuple(new_tuple)

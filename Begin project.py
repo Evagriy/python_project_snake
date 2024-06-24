aa56cb4a9de4b23ccd329f6895183c51e10f2a4f
Evagriy <egorkoleda56@gmail.com>
2024-06-24 15:28:51 +0300

    without TailCollisionException

--- Begin project.py
+++ Begin project.py
@@ -2,13 +2,17 @@ from typing import List, Tuple
 
 list_numbers = [(2, 4), (5, 7)]
 new_tuple = (1, 2)
-eating:  bool = False
-not_eating: bool = True
+eating:  bool = True
+
+def check_tuple(list_numbers: List[tuple[int, int]], new_tuple: Tuple[int, int]) -> bool:
+    return new_tuple in list_numbers
+
 def add_tuple(new_tuple: Tuple[int, int]) -> None:
 
     list_numbers.append(new_tuple)
-    if not_eating:
-        del list_numbers[0]
+    print(check_tuple(list_numbers, new_tuple))
+    if eating:
+         del list_numbers[0]
 
     print(list_numbers)
 

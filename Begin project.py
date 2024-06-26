da63974e0953a372905346ca66cfb25f82603ca6
Evagriy <egorkoleda56@gmail.com>
2024-06-26 12:32:52 +0300

    without TailCollisionException

--- Begin project.py
+++ Begin project.py
@@ -4,15 +4,30 @@ list_numbers = [(2, 4), (5, 7)]
 new_tuple = (1, 2)
 eating:  bool = True
 
+class tuple_exists_error(Exception):
+    pass
+
+
 def check_tuple(list_numbers: List[tuple[int, int]], new_tuple: Tuple[int, int]) -> bool:
-    return new_tuple in list_numbers
+    if new_tuple in list_numbers:
+        raise tuple_exists_error("game over")
+    return False
 
 def add_tuple(new_tuple: Tuple[int, int]) -> None:
 
-    list_numbers.append(new_tuple)
-    print(check_tuple(list_numbers, new_tuple))
-    if eating:
-         del list_numbers[0]
+    try:
+        check_tuple(list_numbers, new_tuple)
+        list_numbers.append(new_tuple)
+        print(f"tuple {new_tuple} add in list.")
+    except tuple_exists_error as e:
+        print(e)
+        return
+
+    if eating == True:
+        print("Похавал")
+    elif eating == False:
+        del list_numbers[0]
+        print("Голодуха")
 
     print(list_numbers)
 
